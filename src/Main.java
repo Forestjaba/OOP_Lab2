0683b44de5a32bd3043c30c6dc0f5a3220aa7ffe
Forestjaba <skartem0106@mail.ru>
2024-01-20 00:04:10 +0600

    1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
 import java.util.regex.*;
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String s = scan.nextLine();
         String regex = "abcdefghijklmnopqrstuv18340";
-        String s = "abcdefghijklmnopqrstuv18340";
         Pattern p2 = Pattern. compile (regex);
         Matcher m2 = p2.matcher(s);
         boolean b1 = m2.matches();
-        System.out.println(b1);
+        if (b1 == true)
+            System.out.println("Выражение правильное");
+        else
+            System.out.println("Выражение неправильное");
     }
 }
